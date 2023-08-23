42e28acd7e22b675f38400831199066426a0cf9d
Zain Zafar <zainzafar@Zains-MacBook-Pro.local>
2023-08-23 18:20:45 +0100

    ai analysis added

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,31 +1,42 @@
-import Editor from "@/components/Editor";
-import { getUserByClerkId } from "@/utils/auth";
-import { prisma } from "@/utils/db";
+import Editor from '@/components/Editor'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
 
 type EntryPageProps = {
-    params: {
-      id: string;
-    };
-  };
-
-  const getEntry = async (id: string) => {
-    const user = await getUserByClerkId({select: {id: true}})
-    return await prisma.journalEntry.findUnique({
-        where: {
-          userId_id: {
-            userId: user.id,
-            id,
-          },
-        },
-      })
+  params: {
+    id: string
   }
+}
+
+const getEntry = async (id: string) => {
+  const user = await getUserByClerkId({ select: { id: true } })
+  return await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+     
+    },
+    include: {
+        analysis: true,
+      },
+  })
+}
 
 export default async function EntryPage({ params }: EntryPageProps) {
-    const entry = await getEntry(params.id)
+  const entry = await getEntry(params.id)
+  console.log(entry)
+  const { mood, summary, color, subject, negative } = entry?.analysis
+  const analysisData = [
+    { name: 'Summary', value: summary },
+    { name: 'Subject', value: subject },
+    { name: 'Mood', value: mood },
+    { name: 'Negative', value: negative ? 'True' : 'False' },
+  ]
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full ">
       <Editor entry={entry} />
     </div>
   )
-  }
-  
+}
--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,3 +1,4 @@
+import { analyze } from '@/utils/ai'
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import { NextResponse } from 'next/server'
@@ -16,6 +17,19 @@ export const PATCH = async (request: Request, { params }) => {
       content,
     },
   })
+  const analysis = await analyze(updatedEntry.content)
 
-  return NextResponse.json({ data: updatedEntry })
+  const updated = await prisma.analysis.upsert({
+    where: {
+      entryId: updatedEntry.id,
+    },
+    create: {
+      entryId: updatedEntry.id,
+      ...analysis,
+    },
+    update: analysis,
+  })
+
+
+  return NextResponse.json({ data: { ...updatedEntry, analysis: updated } })
 }
